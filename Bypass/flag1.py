3edef81a8e6b1b2ac212818af8053936e56cf2c0
DukeOfNero <138586716+DukeOfNero@users.noreply.github.com>
2024-07-12 13:37:31 +0200

    Update flag1.py

--- Bypass/flag1.py
+++ Bypass/flag1.py
@@ -1,1 +1,14 @@
-Bypass
+import socket
+
+target_host = 'cctv.thm'
+target_port = 5000
+
+message = 'Hello, CCTV!'.encode()
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock.bind(('', 5000))
+
+sock.sendto(message, (target_host, target_port))
+
+sock.close()
